e8554677c15cfbc14404152677b6fb65c97f4327
marlinhodev <marloncavalcanti1@gmail.com>
2022-04-25 19:52:34 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/marlinhodev/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.marlinhodev.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marlinhodev.workshopmongo.domain.User;
+import com.marlinhodev.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}
